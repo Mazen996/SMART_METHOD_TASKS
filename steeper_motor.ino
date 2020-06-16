8d6ca20ca13995686c09aebfec53153e27011952
Mazen996 <66920904+Mazen996@users.noreply.github.com>
2020-06-16 22:44:25 +0300

    TASK 3 STEEPER MOTOR CODE

---
+++ steeper_motor.ino
@@ -0,0 +1,58 @@
+// to include Arduino stepper motor library
+#include <Stepper.h>
+ 
+// change this to the number of steps on your motor
+#define STEPS 20
+ 
+// the number of steps of the motor and the pins used 
+Stepper stepper(STEPS, 8, 9, 10, 11);
+ 
+const int button =  4; // direction control button is connected to Arduino pin 4
+const int pot    = A0; // speed control potentiometer is connected to analog pin 0
+ 
+void setup()
+{
+  // configure button pin as input with internal pull up enabled
+  pinMode(button, INPUT_PULLUP);
+}
+ 
+int direction_ = 1, speed_ = 0;
+ 
+void loop()
+{
+  if ( digitalRead(button) == 0 )  // if button is pressed
+    if ( debounce() )  // debounce button signal
+    {
+      direction_ *= -1;  // reverse direction 
+      while ( debounce() ) ;  // waits for button release
+    }
+ 
+  // read analog value from the potentiometer
+  int val = analogRead(pot);
+ 
+  // map digital value from [0, 1023] to [5, 100]
+  // ===> min speed = 5 and max speed = 100 rpm
+  if ( speed_ != map(val, 0, 1023, 5, 100) )
+  { // if the speed was changed
+    speed_ = map(val, 0, 1023, 5, 100);
+    // set the speed of the motor
+    stepper.setSpeed(speed_);
+  }
+ 
+  // move the stepper motor
+  stepper.step(direction_);
+ 
+}
+ 
+// a small function for button debounce
+bool debounce()
+{
+  byte count = 0;
+  for(byte i = 0; i < 5; i++) {
+    if (digitalRead(button) == 0)
+      count++;
+    delay(10);
+  }
+  if(count > 2)  return 1;
+  else           return 0;
+}
